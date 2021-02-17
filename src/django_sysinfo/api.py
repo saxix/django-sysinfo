94b2ae59cc318893604ac25ecf69b0d032230630
sax <s.apostolico@gmail.com>
2021-02-17 17:18:20 +0100

    updates

--- src/django_sysinfo/api.py
+++ src/django_sysinfo/api.py
@@ -9,8 +9,7 @@ import sys
 import tempfile
 from collections import OrderedDict
 
-from django.utils.regex_helper import _lazy_re_compile
-from django.views.debug import SafeExceptionReporterFilter
+from django.utils.functional import SimpleLazyObject
 from pkg_resources import get_distribution
 
 from django.conf import settings
@@ -27,6 +26,23 @@ from .conf import config
 logger = logging.getLogger(__name__)
 
 UNKNOWN = "unknown"
+
+
+def _lazy_re_compile(regex, flags=0):
+    """Lazily compile a regex with flags."""
+
+    def _compile():
+        # Compile the regex if it was not passed pre-compiled.
+        if isinstance(regex, (str, bytes)):
+            return re.compile(regex, flags)
+        else:
+            assert not flags, (
+                'flags must be empty if regex is passed pre-compiled'
+            )
+            return regex
+
+    return SimpleLazyObject(_compile)
+
 
 hidden_settings = _lazy_re_compile(config.masked_environment, flags=re.I)
 cleansed_substitute = '********************'
