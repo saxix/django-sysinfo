ff9544ada22fd31bf125c106dcd28fcc983851c5
sax <s.apostolico@gmail.com>
2021-08-05 14:46:05 +0200

    minor enhancements

--- src/django_sysinfo/urls.py
+++ src/django_sysinfo/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 from django_sysinfo.views import check, echo, http_basic_login, sysinfo, version
 
 urlpatterns = (
-    path("info/$", http_basic_login(sysinfo), name="sys-info"),
-    path("version/<str:name>/$", http_basic_login(version), name="sys-version"),
-    path("echo/<str:value>/$", echo, name="sys-echo"),
-    path("check/<str:id>/$", http_basic_login(check), name="sys-check"),
+    path("info/", http_basic_login(sysinfo), name="sys-info"),
+    path("version/<str:name>/", http_basic_login(version), name="sys-version"),
+    path("echo/<str:value>/", echo, name="sys-echo"),
+    path("check/<str:id>/", http_basic_login(check), name="sys-check"),
 )
